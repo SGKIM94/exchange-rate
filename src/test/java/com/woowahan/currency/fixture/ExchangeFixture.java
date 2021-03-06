a05ad1dee58253dac36b17f186a0616cd12a629c
sanggu <tkdrn8578@naver.com>
2021-03-06 18:11:28 +0900

    refactor: 전부 JPY로 되어있던 오타 수정

--- src/test/java/com/woowahan/currency/fixture/ExchangeFixture.java
+++ src/test/java/com/woowahan/currency/fixture/ExchangeFixture.java
@@ -6,10 +6,12 @@ import com.woowahan.currency.dto.QuotesDto;
 import static com.woowahan.currency.domain.Countries.USD;
 
 public class ExchangeFixture {
+    public static final double TEST_USD_KRW = 84.095497;
+
     private static final QuotesDto TEST_QUOTES_DTO = QuotesDto.builder()
-            .USDJPY(84.095497)
+            .USDKRW(TEST_USD_KRW)
             .USDJPY(0.123133)
-            .USDJPY(2.192873)
+            .USDPHP(2.192873)
             .build();
 
     public static final CurrencyResponseDto TEST_CURRENCY = CurrencyResponseDto.builder()
