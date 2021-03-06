4bf8c7794699672b9906edae4e75ca05a682d857
sanggu <tkdrn8578@naver.com>
2021-03-06 15:34:36 +0900

    feat: 환율정보의 Property에 대한 테스트코드 추가

---
+++ src/test/java/com/woowahan/currency/properties/ExchangeApiPropertyTest.java
@@ -0,0 +1,27 @@
+package com.woowahan.currency.properties;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ExchangeApiPropertyTest {
+
+    @Autowired
+    private ExchangeApiProperty property;
+
+    @Test
+    @DisplayName("환율정보를 조회하기 위한 API의 URL 을 가져온다")
+    void getExchangeApiUrl() {
+        String url = property.getUrl();
+
+        assertThat(url).isEqualTo("http://www.apilayer.net/api/live?access_key=ee50cd7cc73c9b7a7bb3d9617cfb6b9c");
+    }
+}
