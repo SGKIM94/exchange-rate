2bbcba62af57143f9dfa2ec52ec7e631c571d3eb
sanggu <tkdrn8578@naver.com>
2021-03-06 19:00:56 +0900

    feat: 필수값, 최소값, 최대값에 대한 validation 추가

--- src/main/java/com/woowahan/currency/dto/CalculateRecipientRequestDto.java
+++ src/main/java/com/woowahan/currency/dto/CalculateRecipientRequestDto.java
@@ -3,19 +3,27 @@ package com.woowahan.currency.dto;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 @NoArgsConstructor
 public class CalculateRecipientRequestDto {
-    private String recipientCountry;
+    @NotEmpty(message = "환율이 존재하지 않습니다")
+    private double exchangeRate;
+
+    @Max(value = 10000, message = "송금액이 바르지 않습니다")
+    @Min(value = 0, message = "송금액이 바르지 않습니다")
     private int transferAmount;
 
     @Builder
-    public CalculateRecipientRequestDto(String recipientCountry, int transferAmount) {
-        this.recipientCountry = recipientCountry;
+    public CalculateRecipientRequestDto(double exchangeRate, int transferAmount) {
+        this.exchangeRate = exchangeRate;
         this.transferAmount = transferAmount;
     }
 
-    public String getRecipientCountry() {
-        return recipientCountry;
+    public double getExchangeRate() {
+        return exchangeRate;
     }
 
     public double getTransferAmount() {
