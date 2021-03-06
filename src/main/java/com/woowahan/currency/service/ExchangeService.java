b8545f55e30ef1e55d5f3f6d71dd11150664d7a7
sanggu <tkdrn8578@naver.com>
2021-03-07 00:16:36 +0900

    refactor: exchangeUrl을 만드는 것을 extract method로 추출

--- src/main/java/com/woowahan/currency/service/ExchangeService.java
+++ src/main/java/com/woowahan/currency/service/ExchangeService.java
@@ -3,6 +3,7 @@ package com.woowahan.currency.service;
 import com.woowahan.currency.dto.CalculateRecipientRequestDto;
 import com.woowahan.currency.dto.CalculateRecipientResponseDto;
 import com.woowahan.currency.dto.CurrencyResponseDto;
+import com.woowahan.currency.dto.RetrieveExchangeResponseDto;
 import com.woowahan.currency.external.ExchangeApiClient;
 import com.woowahan.currency.properties.ExchangeApiProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,14 @@ public class ExchangeService {
         return CalculateRecipientResponseDto.toDto(request.getExchangeRate() * request.getTransferAmount());
     }
 
-    public CurrencyResponseDto retrieveExchanges() {
-        return exchangeApiClient.retrieve(apiProperty.getUrl());
+    public RetrieveExchangeResponseDto retrieveExchanges(String source, String destination) {
+        String url = makeExchangeUrl(apiProperty.getUrl(), source, destination);
+        CurrencyResponseDto retrieve = exchangeApiClient.retrieve(url);
+
+        return RetrieveExchangeResponseDto.toDto(retrieve, source, destination);
+    }
+
+    private String makeExchangeUrl(String url, String source, String destination) {
+        return url + "&source=" + source + "&currencies=" + destination;
     }
 }
