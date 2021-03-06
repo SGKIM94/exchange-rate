a754a0c35d9d0858d8d58cb5737705caddbcf38e
sanggu <tkdrn8578@naver.com>
2021-03-06 17:46:56 +0900

    feat: 환율정보를 토대로 수취금액을 계산하여 전달하는 Acceptance 테스트코드 추가

--- src/test/java/com/woowahan/currency/controller/ExchangeAcceptanceTest.java
+++ src/test/java/com/woowahan/currency/controller/ExchangeAcceptanceTest.java
@@ -5,10 +5,22 @@ import org.junit.jupiter.api.Test;
 
 class ExchangeAcceptanceTest extends AbstractAcceptanceTest {
 
+    private static final String BASE_EXCHANGE_URL = "/exchange";
+
     @Test
-    @DisplayName("환율 정보를 전달해준다.")
-    void retrieveExchange() {
-        webTestClient.get().uri("/exchange")
+    @DisplayName("환율정보를 전달해준다")
+    void retrieveExchange_test() {
+        webTestClient.get().uri(BASE_EXCHANGE_URL)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody();
+    }
+
+    @Test
+    @DisplayName("수취금액을 계산하여 전달한다")
+    void calculateRecipientAmount_test() {
+        webTestClient.post().uri(BASE_EXCHANGE_URL + "/recipient/amount")
                 .exchange()
                 .expectStatus()
                 .isOk()
