fac7b83f9f97ed1296a6cd3d412916cc12a308ce
sanggu <tkdrn8578@naver.com>
2021-03-07 00:15:30 +0900

    refactor: Controller에 대한 index를 보여주는 controller 추가

--- src/main/java/com/woowahan/currency/controller/ExchangeController.java
+++ src/main/java/com/woowahan/currency/controller/ExchangeController.java
@@ -1,37 +1,15 @@
 package com.woowahan.currency.controller;
 
 import com.woowahan.currency.dto.CalculateRecipientRequestDto;
-import com.woowahan.currency.dto.CalculateRecipientResponseDto;
-import com.woowahan.currency.dto.CurrencyResponseDto;
-import com.woowahan.currency.service.ExchangeService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-@RequestMapping("/exchange")
 public class ExchangeController {
 
-    private final ExchangeService exchangeService;
-
-    public ExchangeController(ExchangeService exchangeService) {
-        this.exchangeService = exchangeService;
-    }
-
     @GetMapping
-    public ResponseEntity retrieveExchanges() {
-        CurrencyResponseDto currency = exchangeService.retrieveExchanges();
-        return ResponseEntity.ok(currency);
-    }
-
-    @PostMapping("/recipient/amount")
-    public ResponseEntity calculateRecipientAmount(@Valid @RequestBody CalculateRecipientRequestDto request) {
-        CalculateRecipientResponseDto recipientAmount = exchangeService.calculateRecipientAmount(request);
-        return ResponseEntity.ok(recipientAmount);
+    public String home(@ModelAttribute("exchange") CalculateRecipientRequestDto exchange) {
+        return "index";
     }
 }
