7e1c55a2a908fe4497382f646f156bd199551be9
sanggu <tkdrn8578@naver.com>
2021-03-06 18:10:19 +0900

    feat: 환율정보를 받아서 수취국가에 따라 수취금액을 계산해서 전달하는 calculateRecipientAmount 추가

--- src/main/java/com/woowahan/currency/service/ExchangeService.java
+++ src/main/java/com/woowahan/currency/service/ExchangeService.java
@@ -1,10 +1,14 @@
 package com.woowahan.currency.service;
 
+import com.woowahan.currency.dto.CalculateRecipientRequestDto;
+import com.woowahan.currency.dto.CalculateRecipientResponseDto;
 import com.woowahan.currency.dto.CurrencyResponseDto;
 import com.woowahan.currency.external.ExchangeApiClient;
 import com.woowahan.currency.properties.ExchangeApiProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.woowahan.currency.domain.Countries.*;
 
 @Slf4j
 @Service
@@ -15,6 +19,23 @@ public class ExchangeService {
     public ExchangeService(ExchangeApiClient exchangeApiClient, ExchangeApiProperty apiProperty) {
         this.exchangeApiClient = exchangeApiClient;
         this.apiProperty = apiProperty;
+    }
+
+    public CalculateRecipientResponseDto calculateRecipientAmount(CalculateRecipientRequestDto request) {
+        CurrencyResponseDto currencyResponse = retrieveExchanges();
+
+        if (request.getRecipientCountry().equals(KRW)) {
+            return CalculateRecipientResponseDto
+                    .toDto(currencyResponse.getQuotes().getUSDKRW() * request.getTransferAmount());
+        } else if (request.getRecipientCountry().equals(JPY)) {
+            return CalculateRecipientResponseDto
+                    .toDto(currencyResponse.getQuotes().getUSDJPY() * request.getTransferAmount());
+        } else if (request.getRecipientCountry().equals(PHP)) {
+            return CalculateRecipientResponseDto
+                    .toDto(currencyResponse.getQuotes().getUSDPHP() * request.getTransferAmount());
+        }
+
+        throw new IllegalArgumentException("잘못된 수취국가입니다.");
     }
 
     public CurrencyResponseDto retrieveExchanges() {
