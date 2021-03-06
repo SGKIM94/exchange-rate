70a2633e0358b1de9bd70f037f644f2f942beb3c
sanggu <tkdrn8578@naver.com>
2021-03-07 00:16:08 +0900

    refactor: quotes의 타입 변경에 따른 테스트코드 수정

--- src/test/java/com/woowahan/currency/external/ExchangeApiClientTest.java
+++ src/test/java/com/woowahan/currency/external/ExchangeApiClientTest.java
@@ -1,7 +1,6 @@
 package com.woowahan.currency.external;
 
 import com.woowahan.currency.dto.CurrencyResponseDto;
-import com.woowahan.currency.dto.QuotesDto;
 import com.woowahan.currency.properties.ExchangeApiProperty;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Map;
+
 import static com.woowahan.currency.domain.Countries.USD;
+import static com.woowahan.currency.fixture.ExchangeFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -28,12 +30,12 @@ class ExchangeApiClientTest {
     void retrieve_test() {
         CurrencyResponseDto response = exchangeApiClient.retrieve(property.getUrl());
 
-        QuotesDto quotes = response.getQuotes();
+        Map<String, Double> quotes = response.getQuotes();
 
         assertThat(response.isSuccess()).isTrue();
         assertThat(response.getSource()).isEqualTo(USD.getName());
-        assertThat(quotes.getUSDJPY()).isNotNull();
-        assertThat(quotes.getUSDKRW()).isNotNull();
-        assertThat(quotes.getUSDPHP()).isNotNull();
+        assertThat(quotes.get(USDKRW_KEY)).isNotNull();
+        assertThat(quotes.get(USDJPY_KEY)).isNotNull();
+        assertThat(quotes.get(USDPHP_KEY)).isNotNull();
     }
 }
