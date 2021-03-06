28ad853c37b6a36d311eca0185ebf7436fa0192f
sanggu <tkdrn8578@naver.com>
2021-03-06 17:47:15 +0900

    feat: Mock calculateRecipientAmount API 추가

--- src/main/java/com/woowahan/currency/controller/ExchangeController.java
+++ src/main/java/com/woowahan/currency/controller/ExchangeController.java
@@ -1,14 +1,31 @@
 package com.woowahan.currency.controller;
 
+import com.woowahan.currency.dto.CurrencyResponseDto;
+import com.woowahan.currency.service.ExchangeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/exchange")
 public class ExchangeController {
 
-    @GetMapping("/exchange")
+    private final ExchangeService exchangeService;
+
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
+    @GetMapping
     public ResponseEntity retrieveExchanges() {
+        CurrencyResponseDto currency = exchangeService.retrieveExchanges();
+        return ResponseEntity.ok(currency);
+    }
+
+    @PostMapping("/recipient/amount")
+    public ResponseEntity calculateRecipientAmount() {
         return ResponseEntity.ok().build();
     }
 }
