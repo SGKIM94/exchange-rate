a07883d09d9ec204ad3c85157e2b628ef5da265e
sanggu <tkdrn8578@naver.com>
2021-03-06 14:19:19 +0900

    feat: AcceptanceTest에 대한 WebClientTest설정을 가지고 있는 AbstractClass 추가

---
+++ src/test/java/com/woowahan/currency/controller/AbstractAcceptanceTest.java
@@ -0,0 +1,17 @@
+package com.woowahan.currency.controller;
+
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "100000")
+@ExtendWith(SoftAssertionsExtension.class)
+public class AbstractAcceptanceTest {
+
+    @Autowired
+    protected WebTestClient webTestClient;
+}
