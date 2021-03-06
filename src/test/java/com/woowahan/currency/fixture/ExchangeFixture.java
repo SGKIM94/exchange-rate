62c668e9a849a7e0cf4e4011c02e43ac0ffeb04c
sanggu <tkdrn8578@naver.com>
2021-03-07 00:15:03 +0900

    refactor: quete를 가져오는 KEY에 대한 test fixture 추가

--- src/test/java/com/woowahan/currency/fixture/ExchangeFixture.java
+++ src/test/java/com/woowahan/currency/fixture/ExchangeFixture.java
@@ -1,23 +1,31 @@
 package com.woowahan.currency.fixture;
 
 import com.woowahan.currency.dto.CurrencyResponseDto;
-import com.woowahan.currency.dto.QuotesDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.woowahan.currency.domain.Countries.USD;
 
 public class ExchangeFixture {
-    public static final double TEST_USD_KRW = 84.095497;
-
-    private static final QuotesDto TEST_QUOTES_DTO = QuotesDto.builder()
-            .USDKRW(TEST_USD_KRW)
-            .USDJPY(0.123133)
-            .USDPHP(2.192873)
-            .build();
+    public static final double TEST_USD_KRW = 84.123497;
+    public static final String TEST_FORMATTED_USD_KRW = "84.12";
+    public static final String USDKRW_KEY = "USDKRW";
+    public static final String USDJPY_KEY = "USDJPY";
+    public static final String USDPHP_KEY = "USDPHP";
 
     public static final CurrencyResponseDto TEST_CURRENCY = CurrencyResponseDto.builder()
             .source(USD.getName())
             .success(true)
-            .quotes(TEST_QUOTES_DTO)
+            .quotes(getTestQuotesDto())
             .build();
 
+    private static Map<String, Double> getTestQuotesDto() {
+        Map<String, Double> quotes = new HashMap<>();
+        quotes.put(USDKRW_KEY, TEST_USD_KRW);
+        quotes.put(USDJPY_KEY, 2.0123012);
+        quotes.put(USDPHP_KEY, 0.1283712);
+
+        return quotes;
+    }
 }
