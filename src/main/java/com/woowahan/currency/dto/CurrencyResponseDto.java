5c02f498e4ce3d0a0a8a731521cf4c6633b6027d
sanggu <tkdrn8578@naver.com>
2021-03-07 00:13:36 +0900

    refactor: @Getter를 제거하고 get 메서드들 추가

--- src/main/java/com/woowahan/currency/dto/CurrencyResponseDto.java
+++ src/main/java/com/woowahan/currency/dto/CurrencyResponseDto.java
@@ -1,22 +1,41 @@
 package com.woowahan.currency.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+import java.util.Map;
+
 @NoArgsConstructor
 public class CurrencyResponseDto implements ClientResponseDto {
     private boolean success;
     private String source;
-    private QuotesDto quotes;
+    private Map<String, Double> quotes;
 
     @Builder
-    public CurrencyResponseDto(boolean success, String source, QuotesDto quotes) {
+    public CurrencyResponseDto(boolean success, String source, Map<String, Double> quotes) {
         this.success = success;
         this.source = source;
         this.quotes = quotes;
     }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public Map<String, Double> getQuotes() {
+        return quotes;
+    }
+
+    @JsonIgnore
+    Double getQuote(String source, String destination) {
+        return quotes.get(source + destination);
+    }
+
 
     @Override
     public String toString() {
